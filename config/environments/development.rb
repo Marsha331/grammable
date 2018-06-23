6b3288adf9a2b371729b7da5ed2dbbe551d182f9
Marsha331 <marsha331@gmail.com>
2018-06-23 22:57:15 +0000

    Devise

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -31,6 +31,8 @@ Rails.application.configure do
 
   config.action_mailer.perform_caching = false
 
+  config.action_mailer.default_url_options = { host: 'grammable-marsha-swallow.herokuapp.com' }
+
   # Print deprecation notices to the Rails logger.
   config.active_support.deprecation = :log
 
