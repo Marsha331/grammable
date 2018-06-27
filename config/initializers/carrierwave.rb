7bb5361680cdcb71e1c95b690227a5d43bcb7048
Marsha331 <marsha331@gmail.com>
2018-06-27 01:14:04 +0000

    Comments

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -9,8 +9,7 @@ else
   config.fog_credentials = {
       provider:               'AWS',
       aws_access_key_id:      ENV["AWS_ACCESS_KEY"],
-      aws_secret_access_key:  ENV["AWS_SECRET_KEY"],
-      region:                ENV["AWS_REGION"]
+      aws_secret_access_key:  ENV["AWS_SECRET_KEY"]
     }
   config.fog_directory =    ENV["AWS_BUCKET"]
   config.fog_public = false
