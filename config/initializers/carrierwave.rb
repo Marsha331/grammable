44df725209252db8f83648e858da15d741657356
Marsha331 <marsha331@gmail.com>
2018-06-27 00:20:46 +0000

    Validating Image

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -2,16 +2,16 @@
 
 CarrierWave.configure do |config|
 if Rails.env.test?
-    config.storage = :file
-    config.enable_processing = false
-  else
-    config.fog_provider = 'fog/aws'
-    config.fog_credentials = {
+  config.storage = :file
+  config.enable_processing = false
+else
+  config.fog_provider = 'fog/aws'
+  config.fog_credentials = {
       provider:               'AWS',
       aws_access_key_id:      ENV["AWS_ACCESS_KEY"],
       aws_secret_access_key:  ENV["AWS_SECRET_KEY"],
       region:                ENV["AWS_REGION"]
-  }
+    }
   config.fog_directory =    ENV["AWS_BUCKET"]
   config.fog_public = false
   config.storage = :fog
